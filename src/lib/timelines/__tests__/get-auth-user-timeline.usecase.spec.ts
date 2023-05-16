92874219ad6708ee6eb64f055f76f92c59a6a36c
Pierre Criulanscy <pierrecriulanscy@gmail.com>
2023-05-16 17:49:21 +0200

    test: get auth user timeline test structure is written

--- src/lib/timelines/__tests__/get-auth-user-timeline.usecase.spec.ts
+++ src/lib/timelines/__tests__/get-auth-user-timeline.usecase.spec.ts
@@ -1,7 +1,44 @@
 import { describe, it } from "vitest";
 
-describe("test suite", () => {
-  it("it works", ({ expect }) => {
-    expect(true).toBe(false);
+describe("Feature: Retrieving authenticated user's timeline", () => {
+  it("Example: Alice is authenticated and can see her timeline", async ({
+    expect,
+  }) => {
+    //arrange (given)
+    givenAuthenticatedUserIs("Alice");
+    givenExistingTimeline({
+      user: "Alice",
+      messages: [
+        {
+          text: "Hello it's Bob",
+          author: "Bob",
+          publishedAt: new Date("2023-05-16T12:06:00.000Z"),
+        },
+        {
+          text: "Hello it's Alice",
+          author: "Alice",
+          publishedAt: new Date("2023-05-16T12:05:00.000Z"),
+        },
+      ],
+    });
+
+    //act (when)
+    await whenRetrievingAuthenticatedUserTimeline();
+
+    //assert (then)
+    thenTheReceivedTimelineShouldBe({
+      messages: [
+        {
+          text: "Hello it's Bob",
+          author: "Bob",
+          publishedAt: new Date("2023-05-16T12:06:00.000Z"),
+        },
+        {
+          text: "Hello it's Alice",
+          author: "Alice",
+          publishedAt: new Date("2023-05-16T12:05:00.000Z"),
+        },
+      ],
+    });
   });
 });
