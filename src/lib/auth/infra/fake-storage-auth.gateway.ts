6b4674be2638fa07b671e451041414151d50cc9b
Pierre Criulanscy <pierrecriulanscy@gmail.com>
2023-06-05 11:23:34 +0200

    fix: correclty implement fake auth storage gateway with new AuthUser

--- src/lib/auth/infra/fake-storage-auth.gateway.ts
+++ src/lib/auth/infra/fake-storage-auth.gateway.ts
@@ -1,25 +1,26 @@
-import { AuthGateway } from "../model/auth.gateway";
+import { z } from "zod";
+import { AuthGateway, AuthUser } from "../model/auth.gateway";
 import { FakeAuthGateway } from "./fake-auth.gateway";
 
 export class FakeStorageAuthGateway implements AuthGateway {
   constructor(private readonly fakeAuthGateway: FakeAuthGateway) {}
 
-  onAuthStateChanged(listener: (user: string) => void): void {
+  onAuthStateChanged(listener: (user: AuthUser) => void): void {
     this.fakeAuthGateway.onAuthStateChanged(listener);
     this.checkIfAuthenticated();
   }
 
-  async authenticateWithGoogle(): Promise<string> {
+  async authenticateWithGoogle(): Promise<AuthUser> {
     const authUser = await this.fakeAuthGateway.authenticateWithGoogle();
-    localStorage.setItem("fake-auth", authUser);
+    localStorage.setItem("fake-auth", JSON.stringify(authUser));
     this.fakeAuthGateway.onAuthStateChangedListener(authUser);
 
     return authUser;
   }
 
-  async authenticateWithGithub(): Promise<string> {
+  async authenticateWithGithub(): Promise<AuthUser> {
     const authUser = await this.fakeAuthGateway.authenticateWithGithub();
-    localStorage.setItem("fake-auth", authUser);
+    localStorage.setItem("fake-auth", JSON.stringify(authUser));
     this.fakeAuthGateway.onAuthStateChangedListener(authUser);
 
     return authUser;
@@ -27,8 +28,27 @@ export class FakeStorageAuthGateway implements AuthGateway {
 
   private checkIfAuthenticated() {
     const maybeAuthUser = localStorage.getItem("fake-auth");
+
     if (maybeAuthUser !== null) {
-      this.fakeAuthGateway.simulateAuthStateChanged(maybeAuthUser);
+      try {
+        const jsonAuthUser = JSON.parse(maybeAuthUser);
+        const AuthUserSchema = z.object({
+          id: z.string(),
+          username: z.string(),
+          profilePicture: z.string().optional(),
+        });
+        const authUserResult = AuthUserSchema.safeParse(jsonAuthUser);
+        if (authUserResult.success) {
+          const authUser: AuthUser = {
+            id: authUserResult.data.id,
+            username: authUserResult.data.username,
+            profilePicture: authUserResult.data.profilePicture,
+          };
+          this.fakeAuthGateway.simulateAuthStateChanged(authUser);
+        }
+      } catch (err) {
+        console.error(err);
+      }
     }
   }
 }
